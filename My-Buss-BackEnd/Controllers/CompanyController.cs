0e72fee4ad14501ce752062b331dbd0c11646cde
Montes15 <samuelmontes1105@gmail.com>
2024-03-03 23:55:35 -0500

    login company endpoint created

--- My-Buss-BackEnd/Controllers/CompanyController.cs
+++ My-Buss-BackEnd/Controllers/CompanyController.cs
@@ -3,8 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 using My_Buss_BackEnd.Helpers;
 using My_Buss_BackEnd.Models;
 using System.Data;
+using System.Text;
 using System.Data.SqlClient;
+using System.Security.Claims;
 using static My_Buss_BackEnd.Helpers.Constants;
+using API.Models;
 
 namespace My_Buss_BackEnd.Controllers
 {
@@ -13,6 +16,7 @@ namespace My_Buss_BackEnd.Controllers
     public class CompanyController(IConfiguration _config) : ControllerBase
     {
         private readonly SqlConnection _conn = Utils.GetConnection(_config.GetConnectionString("DefaultConnection")!);
+        private readonly byte[] keyBytes = Encoding.ASCII.GetBytes(_config.GetSection("SecurityKey").ToString()!);
         [HttpGet]
         [Route("Listar")]
         public IActionResult GetAllCompanies()
@@ -43,7 +47,7 @@ namespace My_Buss_BackEnd.Controllers
         [Route("Registrar")]
         public IActionResult RegisterCompany(Empresa company)
         {
-            string q = $"EXEC usp_RegistrarEmpresa '{company.IdEmpresa}', '{company.Nombre}', '{company.Ubicacion}', '{company.Telefono}', '{company.Correo_electronico}', '{company.Imagen}', '{company.Contraseña}'";            
+            string q = $"EXEC usp_RegistrarEmpresa '{company.IdEmpresa}', '{company.Nombre}', '{company.Ubicacion}', '{company.Telefono}', '{company.Correo_electronico}', '{company.Imagen}', '{company.Contraseña}'";
 
             Utils.OpenConnection(_conn);
             try
@@ -59,6 +63,44 @@ namespace My_Buss_BackEnd.Controllers
             {
                 Utils.CloseConnection(_conn);
             }
+        }
+
+        [HttpPost]
+        [Route("Ingresar")]
+        public IActionResult ValidateCompany([FromBody] InicioSesion2 request)
+        {
+            string q = $"EXECUTE usp_iniciarSesionEmpresa '{request.Correo}', '{request.Contraseña}'";
+
+
+            Empresa empresa;
+            try
+            {
+                DataTable dt = Utils.GetTableFromQuery(q, _conn);
+                if (dt.Rows.Count < 1) return Unauthorized(new Response(STATUS_MESSAGES.DENIED, "Empresa no encontrada"));
+                empresa = new Empresa(
+                                        dt.Rows[0]["IdEmpresa"].ToString()!, dt.Rows[0]["Nombre"].ToString()!,
+                                       dt.Rows[0]["Ubicacion"].ToString()!, dt.Rows[0]["Telefono"].ToString()!,
+                                       dt.Rows[0]["Correo_electronico"].ToString()!, dt.Rows[0]["Imagen"].ToString()!,
+                                       dt.Rows[0]["Contraseña"].ToString()!
+                                   );
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new Response(STATUS_MESSAGES.ERROR, ex.Message));
+            }
+
+            Claim[] claims = [
+                new("IdEmpresa", empresa.IdEmpresa),
+                new("Nombre", empresa.Nombre),
+                new("Ubicacion", empresa.Ubicacion),
+                new("Telefono", empresa.Telefono),
+                new("Correo_electronico", empresa.Correo_electronico),
+                new("Imagen", empresa.Imagen ?? ""),
+                ];
+            string token = Utils.GenerateToken(claims, keyBytes);
+
+            return Ok(new Response(STATUS_MESSAGES.OK, token));
         }
 
     }
---
+++ My-Buss-BackEnd/Models/InicioSesion2.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Models
+{
+    public class InicioSesion2( string Correo, string contraseña)
+    {
+        public string Correo { get; set; } = Correo;
+        public string Contraseña { get; set; } = contraseña;
+    }
+}
