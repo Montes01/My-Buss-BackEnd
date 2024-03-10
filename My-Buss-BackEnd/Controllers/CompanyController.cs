67c3d5b036c85ed6410196ef4101ef9bdf6cf362
Montes15 <samuelmontes1105@gmail.com>
2024-03-10 13:54:57 -0500

    feat: company can login now

--- My-Buss-BackEnd/Controllers/CompanyController.cs
+++ My-Buss-BackEnd/Controllers/CompanyController.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Data.SqlClient;
 using static My_Buss_BackEnd.Helpers.Constants;
 using System.Data;
+using System.Security.Claims;
 
 namespace My_Buss_BackEnd.Controllers
 {
@@ -71,6 +72,55 @@ namespace My_Buss_BackEnd.Controllers
                 Utils.CloseConnection(_conn);
             }
         }
+
+        [HttpPost]
+        [Route("Ingresar")]
+        public IActionResult LoginCompany([FromBody] InicioSesion request)
+        {
+
+            string q = $"EXECUTE IniciarSesionEmpresa '{request.Correo}', '{request.Contraseña}'";
+            Utils.OpenConnection(_conn);
+            try
+            {
+                var dt = Utils.GetTableFromQuery(q, _conn);
+                if (dt.Rows.Count == 0)
+                {
+                    return BadRequest(new Response(STATUS_MESSAGES.ERROR, "Correo o contraseña incorrectos"));
+                }
+                DataRow row = dt.Rows[0];
+                Empresa company = new()
+                {
+                    ID_Empresa = (int)row["ID_Empresa"],
+                    Nombre = row["Nombre"].ToString()!,
+                    CorreoElectronico = request.Correo,
+                    Contraseña = null,
+                    Logo = row["Logo"].ToString(),
+                    Dirección = row["Dirección"].ToString(),
+                    Teléfono = row["Teléfono"].ToString()!
+                };
+
+                Claim[] claim = [
+                    new Claim("Nombre", company.Nombre),
+                    new Claim("CorreoElectronico", company.CorreoElectronico),
+                    new Claim("ID_Empresa", company.ID_Empresa.ToString()),
+                    new Claim("Logo", company.Logo ?? "empty"),
+                    new Claim("Direccion", company.Dirección ?? "empty"),
+                    new Claim("Telefono", company.Teléfono!)
+                ];
+                string token = Utils.GenerateToken(claim, keyBytes);
+
+                return Ok(new Response(STATUS_MESSAGES.OK, token));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new Response(STATUS_MESSAGES.ERROR, ex.Message));
+            }
+            finally
+            {
+                Utils.CloseConnection(_conn);
+            }
+        }
+
 
 
     }
